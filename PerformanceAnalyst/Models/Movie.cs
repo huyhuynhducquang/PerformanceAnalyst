2bc368976047313f02d9a8e2d4b083d9a3207df1
DESKTOP-MA601IK\OS <huy.huynhducquang@plenti.com.au>
2023-09-26 22:34:55 +0700

    Remove

--- PerformanceAnalyst/Models/Movie.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PerformanceAnalyst.Models
-{
-    public class Movie
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Author { get; set; }
-    }
-}
